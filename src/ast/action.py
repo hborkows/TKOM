be4486f80c2bfcff8a99ec7d21014e2c8636f9ab
hborkows <hubert.borkowski@hotmail.com>
2020-01-05 18:31:37 +0100

    Added action class definition

--- src/ast/action.py
+++ src/ast/action.py
@@ -1,3 +1,4 @@
+from typing import Optional, List
 from src.ast.instruction import Instruction
 from src.lexer.token import Token
 from src.ast.object import Object
@@ -5,9 +6,13 @@ from src.ast.object import Object
 
 class Action(Instruction):
 
-    def __init__(self, object, token: Token):
-        self.object =
+    def __init__(self, token: Token, object1: Object, object2: Object):
+        self.object1 = object1
+        self.object2 = object2
         self.action_name = token.type
 
     def get_representation(self) -> str:
-        return 'Action: '
+        return 'Action: ' + self.action_name.name
+
+    def get_children(self) -> Optional[List]:
+        return [self.object1, self.object2]
